5bc3b04e57613e857bca975f311c06dfeb48ec84
badcoiq <135515279+badcoiq@users.noreply.github.com>
2025-02-16 21:06:19 +0300

    update

---
+++ src/Games/Games.cpp
@@ -0,0 +1,31 @@
+﻿#include "badcoiq.h"
+#include "Games.h"
+
+int main(int argc, char* argv[])
+{
+	Games app;
+	if (app.Init())
+	{
+		app.Run();
+	}
+
+	return EXIT_SUCCESS;
+}
+
+Games::Games()
+{
+}
+
+Games::~Games()
+{
+}
+
+bool Games::Init()
+{
+	return true;
+}
+
+void Games::Run()
+{
+}
+
---
+++ src/Games/Games.h
@@ -0,0 +1,16 @@
+﻿#ifndef __GAMES_H_
+#define __GAMES_H_
+
+#include "badcoiq.h"
+
+class Games
+{
+public:
+	Games();
+	~Games();
+
+	bool Init();
+	void Run();
+};
+
+#endif
